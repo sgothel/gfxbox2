c04320385c767890afb6e8908af0bda8b9305549
Svenson Han Göthel <shg@jausoft.com>
2024-06-12 19:54:08 +0200

    tron: use Tron::Motorrad

--- examples/tron.cpp
+++ examples/tron.cpp
@@ -2,6 +2,7 @@
  * Author: Svenson Han Göthel
  * Funktion Name: Tron.cpp
  */
+#include <tron.hpp>
 #include <pixel/pixel4f.hpp>
 #include <pixel/pixel2f.hpp>
 #include <pixel/pixel2i.hpp>
@@ -11,105 +12,6 @@
 #include <cmath>
 #include <iostream>
 
-class Tron : public pixel::f2::linestrip_t {
-public:
-    const pixel::f2::point_t sp;
-    float angle;
-    float velo;
-    pixel::f2::point_t head;
-    pixel::f2::point_t last;
-    pixel::f2::point_t last_last;
-    pixel::f2::disk_t body;
-
-    Tron(pixel::f2::point_t sp_)
-    : sp(sp_), body(head, 5)
-    {
-        reset();
-    }
-
-    void rotate(float angrad) noexcept override {
-        // std::cout << "rot.pre " << toString() << std::endl ;
-        angle += angrad;
-        if(false){
-            if( pixel::compare(angle, 2*M_PI) >= 0 ) { // p1_angle >= 2*M_PI
-                angle = 0;
-            } else if( pixel::compare(angle, 0) < 0 ) { // p1_angle < 0
-                angle = 1.5f * M_PI;
-            }
-        }
-        p_list.push_back(head);
-        last_last = last;
-        last = head;
-        //std::cout << "rot.post " << toString() << std::endl ;
-    }
-
-    /**
-     *
-     * @param dt in seconds
-     */
-    bool tick(const float dt) noexcept override {
-        pixel::f2::point_t p1_move = pixel::f2::point_t::from_length_angle(velo * dt, angle);
-        // std::cout << "tick " << p1_move.toString() << " + " << head.toString();
-        head += p1_move;
-        // std::cout << " = " << head.toString() << std::endl;
-        body.center = head;
-
-        return true;
-    }
-
-    void reset() noexcept {
-        last = sp;
-        head = sp;
-        last_last = sp;
-        body.center = head;
-        p_list.clear();
-        p_list.push_back(last_last);
-        angle = M_PI_2;
-        velo = 2.0f / 0.016f; // 2 pixel pro 16 ms
-    }
-
-    void draw() const noexcept override {
-        pixel::f2::lineseg_t::draw(last, head);
-        pixel::f2::lineseg_t::draw(last_last, last);
-        pixel::f2::linestrip_t::draw();
-        body.draw(true);
-    }
-
-    void changeSpeed(float a){
-        velo *= a;
-    }
-
-    std::string toString() const noexcept override {
-        return "Tron[h "+head.toString()+", a "+std::to_string(angle)+", v "+std::to_string(velo)+
-                ", tail "+std::to_string(p_list.size())+"]";
-    }
-
-    bool intersects(const Tron& o) const {
-        const pixel::f2::lineseg_t h(head, last);
-        const pixel::f2::lineseg_t o_h(o.head, o.last);
-        return my_intersects(h) ||
-                o.intersects_lineonly(h) ||
-                o_h.intersects(h);
-    }
-private:
-    bool my_intersects(const pixel::f2::lineseg_t & o) const noexcept {
-        if( p_list.size() < 3 ) {
-            return false;
-        }
-        pixel::f2::point_t p0 = p_list[0];
-        for(size_t i=1; i<p_list.size()-1; ++i) {
-            const pixel::f2::point_t& p1 = p_list[i];
-            const pixel::f2::lineseg_t l(p0, p1);
-            if( l.intersects(o) ) {
-                return true;
-            }
-            p0 = p1;
-        }
-        return false;
-    }
-
-};
-
 void mainloop() {
     static const pixel::f4::vec_t text_color(0.5f, 0.5f, 0.5f, 1.0f);
     static const float ax1 = pixel::cart_coord.min_x() + 3 * pixel::cart_coord.width() / 4;
@@ -117,8 +19,8 @@ void mainloop() {
     static const float ay1 = pixel::cart_coord.min_y() + 100;
     static const int text_height = 24;
 
-    static Tron p1(pixel::f2::point_t(ax1, ay1));
-    static Tron p2(pixel::f2::point_t(ax2, ay1));
+    static Tron::Motorrad p1(pixel::f2::point_t(ax2, ay1));
+    static Tron::Motorrad p2(pixel::f2::point_t(ax1, ay1));
     static uint64_t t_last = pixel::getElapsedMillisecond(); // [ms]
     static int a1 = 0;
     static int a2 = 0;
