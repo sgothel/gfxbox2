a4376926d69063dc07ef06ef36e2881f70816f90
Svenson Han Göthel <shg@jausoft.com>
2024-06-29 13:48:20 +0200

    pixel/unit.hpp: renamed Speed to velo(velocity), add to time day, week, month and year,...
    add to length dm, add to mass t and mg,
    add to velo(velocity) km_s and m_h and add accel(acceleration)

--- include/pixel/unit.hpp
+++ include/pixel/unit.hpp
@@ -16,9 +16,16 @@ namespace pixel {
     typedef float si_length_t;
     /** Mass in fractions of kilograms. */
     typedef float si_mass_t;
-    /** Speed in fractions of meter/seconds. */
-    typedef float si_speed_t;
+    /** Velocity in fractions of meter/seconds. */
+    typedef float si_velo_t;
+    /** Acceleration in fractions of meter/seconds^2. */
+    typedef float si_accel_t;
 
+    constexpr si_time_t operator ""_year(unsigned long long int __v)   { return (si_time_t)__v*365.25*24*3600.0; }
+    constexpr si_time_t operator ""_month(unsigned long long int __v)   { return (si_time_t)__v*30*24*3600.0; }
+    constexpr si_time_t operator ""_week(unsigned long long int __v)   { return (si_time_t)__v*7.0*24.0*3600.0; }
+    constexpr si_time_t operator ""_day(unsigned long long int __v)   { return (si_time_t)__v*24.0*3600.0; }
+    
     constexpr si_time_t operator ""_h(unsigned long long int __v)   { return (si_time_t)__v*3600.0; }
     constexpr si_time_t operator ""_min(unsigned long long int __v)   { return (si_time_t)__v*60.0; }
     constexpr si_time_t operator ""_s(unsigned long long int __v)   { return (si_time_t)__v; }
@@ -26,15 +33,25 @@ namespace pixel {
 
     constexpr si_length_t operator ""_km(unsigned long long int __v)   { return (si_length_t)__v*1000.0; }
     constexpr si_length_t operator ""_m(unsigned long long int __v)   { return (si_length_t)__v; }
+    constexpr si_length_t operator ""_dm(unsigned long long int __v)   { return (si_length_t)__v/10.0; }
     constexpr si_length_t operator ""_cm(unsigned long long int __v)   { return (si_length_t)__v/100.0; }
     constexpr si_length_t operator ""_mm(unsigned long long int __v)   { return (si_length_t)__v/1000.0; }
 
+    constexpr si_mass_t operator ""_t(unsigned long long int __v)   { return (si_mass_t)__v * 1000.0; }
     constexpr si_mass_t operator ""_kg(unsigned long long int __v)   { return (si_mass_t)__v; }
     constexpr si_mass_t operator ""_g(unsigned long long int __v)   { return (si_mass_t)__v/1000.0; }
+    constexpr si_mass_t operator ""_mg(unsigned long long int __v)   { return (si_mass_t)__v/1000000.0; }
 
-    constexpr si_mass_t operator ""_m_s(unsigned long long int __v)   { return (si_mass_t)__v; }
-    constexpr si_mass_t operator ""_km_h(unsigned long long int __v)   { return (si_mass_t)__v / 3.6; }
+    constexpr si_velo_t operator ""_km_s(unsigned long long int __v)   { return (si_velo_t)__v * 1000.0; }
+    constexpr si_velo_t operator ""_m_s(unsigned long long int __v)   { return (si_velo_t)__v; }
+    constexpr si_velo_t operator ""_km_h(unsigned long long int __v)   { return (si_velo_t)__v / 3.6; }
+    constexpr si_velo_t operator ""_m_h(unsigned long long int __v)   { return (si_velo_t)__v / 3600.0; }
 
+    constexpr si_accel_t operator ""_km_s2(unsigned long long int __v)   { return (si_accel_t)__v * 1000; }
+    constexpr si_accel_t operator ""_m_s2(unsigned long long int __v)   { return (si_accel_t)__v; }
+    constexpr si_accel_t operator ""_dm_s2(unsigned long long int __v)   { return (si_accel_t)__v / 10; }
+    constexpr si_accel_t operator ""_cm_s2(unsigned long long int __v)   { return (si_accel_t)__v / 100; }
+    constexpr si_accel_t operator ""_mm_s2(unsigned long long int __v)   { return (si_accel_t)__v / 1000; }
 }
 
 #endif /* INCLUDE_PIXEL_UNIT_HPP_ */
